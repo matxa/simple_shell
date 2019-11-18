67590e47b63e8c07c639b852fcaa00a3211eb59a
matxa <matxa21@gmail.com>
2019-11-18 23:57:23 +0000

    need to fix pointers and data types

--- simpleshell.h
+++ simpleshell.h
@@ -10,5 +10,7 @@ int n_strlen(char *str);
 char *n_strtoke(char *str, const char *delim);
 char *_strcpy(char *dest, char *src);
 char *user_input(void);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t _getline(char **buff, size_t *size);
 
 #endif
--- user_input.c
+++ user_input.c
@@ -1,22 +1,5 @@
 #include "simpleshell.h"
 
-ssize_t _getline(char **buff)
-{
-	size_t buff_size = 4096;
-	ssize_t read_size = 0;
-
-	*buff = malloc(buff_size);	
-	read_size = read(0, *buff, buff_size);
-	return read_size;
-}
-
-
-/**
-* get_user_input - uses getline() to get user input
-* Return: getlines returns
-*
-*/
-
 char *user_input(void)
 {
 	ssize_t count_char;
@@ -32,7 +15,7 @@ char *user_input(void)
 	}
 	count_char = getline(&buffer, &bsize, stdin);
 */
-	count_char = _getline(&buffer);
+	count_char = _getline(&buffer, bsize);
 	return (buffer);
 }
 
