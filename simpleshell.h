2adc33c96a90c394a1564be67d9122b278bc6f3f
matxa <matxa21@gmail.com>
2019-11-18 20:13:52 +0000

    add get user input function

--- simpleshell.h
+++ simpleshell.h
@@ -3,7 +3,12 @@
 #include <strings.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
 int n_strlen(char *str);
 char *n_strtoke(char *str, const char *delim);
 char *_strcpy(char *dest, char *src);
+int get_user_input(void);
+
 #endif
