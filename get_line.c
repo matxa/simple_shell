51683f15ab742df0adccded73b6b5e2c0bf18e37
matxa <matxa21@gmail.com>
2019-11-19 15:02:27 +0000

    add error checks

--- get_line.c
+++ get_line.c
@@ -8,25 +8,25 @@
 *
 */
 
-ssize_t _getline(char **buff, size_t *size)
+ssize_t _getline(char **buff)
 {
-	if (buff == NULL || size == NULL)
+	if (buff == NULL)
 		return (-1);
 
 	/* buffer to realloc */
-	char *buff_realloc;
+	/* char **buff_realloc; */
 	/* size of buffer (buff) */
 	size_t buff_size = 4096;
 	/* the number of bytes read is returned */
-	ssize_t read_size = 0;
+	ssize_t read_size;
 	/* malloc for buffer */
 	*buff = malloc(buff_size);
 	/* check if malloc was successful */
-	if (*buff == NULL)
+	/*if (buff == NULL)
 	{
-		perror("Unable to allocate for buff");
+		perror("failed to malloc");
 		return (-1);
-	}
+	}*/
 	read_size = read(0, *buff, buff_size);
 	/* check for read */
 	if (read_size == -1)
@@ -35,13 +35,14 @@ ssize_t _getline(char **buff, size_t *size)
 		return (-1);
 	}
 	/* realloc in case we need more momory */
-	if ((read_size + 1) < buff_size)
+	/*if ((read_size + 1) < buff_size)
 	{
-		buff_realloc = _realloc(*buff, size, buff_size);
+		*buff_realloc = _realloc(*buff, *size, buff_size);
 		if (buff_realloc == NULL)
 			return (-1);
-		*buff = buff_realloc;
-		/* *size = buff_size; */
-	}
+		*buff = *buff_realloc;
+		*size = buff_size;
+	}*/
+
 	return (read_size);
 }
