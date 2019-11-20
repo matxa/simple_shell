eedabf568d519814b01577bd02f4eca795c7a48e
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-20 15:42:11 +0000

    test while loop

---
+++ shellmain.c
@@ -0,0 +1,20 @@
+#include "sampleh.h"
+/**
+ * main - command line interpreter
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: something
+ */
+int main(int ac, char **av, char **env)
+{
+//	if (*av)
+//	{
+//		write(1, *av, sizeof(*av));
+//	}
+//	else 
+//		return (-1);
+//	return(0);
+
+	while (1)
+	{
+}
