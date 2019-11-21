bad18a26b88c934cb33b15def9e17fac271e7eec
matxa <matxa21@gmail.com>
2019-11-21 00:25:05 +0000

    testing user input func

--- user_input.c
+++ user_input.c
@@ -2,32 +2,16 @@
 
 char *user_input(void)
 {
-	ssize_t count_char;
-	int i;
-	size_t bsize = 1024;
-	char *buffer;
-/*
-	buffer = malloc(sizeof(char)* bsize);
-	if (buffer == NULL)
-	{
-		perror("failed to allocate buffer");
-		exit(1);
-	}
+	char *buffer = NULL;
+	size_t bsize = 0;
+	/* check for get line */
+	int count_char;
+
 	count_char = getline(&buffer, &bsize, stdin);
-*/
-	count_char = _getline(&buffer);
+	if (count_char == -1)
+		perror("getline() failed ( count_char == -1 )");
+
+	/* count_char = _getline(&buffer); */
+
 	return (buffer);
 }
-
-/*
-int main(void)
-{
-	char *buffer;
-
-	buffer = user_input();
-	write(1, buffer, sizeof(buffer));
-
-	free(buffer);
-	return (0);
-}
-*/
