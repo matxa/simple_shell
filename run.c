9f0e674f171148897c1c40de9d1fe026c8d5d1ec
matxa <matxa21@gmail.com>
2019-11-25 15:43:30 +0000

    managing user input

--- run.c
+++ run.c
@@ -9,32 +9,51 @@
 
 extern char** environ;
 
+
 int run(char **args)
 {
 	int status;
 	pid_t pid;
 	pid_t wait_for_child;
+	int i;
 	/*char *buffer = tokens(args);*/
 	char *buffer = _getenv("PATH");
-	char *path;
-	char *cp;
-	char *new;
+	char *path = malloc(sizeof(char) * 1024);
+	char *cp = malloc(sizeof(char) * 1024);
+	char *new = malloc(sizeof(char) * 1024);
 	/* creating a child processes */
+	printf("args -> %s\n", *args);
+	printf("buffer -> %s\n", buffer);
+
 	pid = fork();
 	if (pid == 0)
 	{
-		while (buffer != NULL)
+		path = strtok(buffer, ":");
+		printf("path -> %s\n", path);
+		printf("%s\n", "forking");
+		/*cp = strcat(*args, "/");
+		printf("cp -> %s <- the slash\n", cp);*/
+		while (path)
 		{
-			path = strtok(buffer, ":");
-			cp = _strcat("/", args);
-			new = _strcat(path, cp);
-			path = strtok(NULL, ":");
+			cp = strcat("/", *args);
+			printf("cp -> %s <- the slash\n", cp);
+			printf("%s\n", "we in the while loop :)");
+			new = strcat(path, cp);
+			printf("new -> %s\n", new);
+
 			if (access(new, (R_OK | X_OK)) == 0)
+			{
+				printf("%s\n", "exec");
 				execve(new, args, environ);
+			}
+			path = strtok(NULL, ":");
+			i++;
+			printf("the loop runs %d times!", i);
 		}
 	}
 	else
 	{
+		printf("%s\n", "we in the parent process :)");
 		wait_for_child = waitpid(pid, &status, 0);
 	}
 
@@ -48,11 +67,11 @@ int run(char **args)
 *
 */
 
-int execute(char **args)
+/*int execute(char **args)
 {
 	return (run(args));
 }
-
+*/
 /*int main(char **args)
 {
 	printf("$ \n");
--- simpleshell.h
+++ simpleshell.h
@@ -1,11 +1,13 @@
 #ifndef SIMPLESHELL_H
 #define SIMPLESHELL_H
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+/* environ declearation */
 
 int n_strlen(char *str);
 char *n_strtoke(char *str, const char *delim);
@@ -14,9 +16,12 @@ char *user_input(void);
 ssize_t _getline(char **buff);
 void *_realloc(void *ptr, size_t old_size, size_t new_size);
 char **tokens(char *buffer);
-int run(char **args);
-int execute(char **args);
 int my_cd(char **args);
+int run(char **args);
 int my_exit(int n);
+char *_getenv(char *var_name);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
 
 #endif
