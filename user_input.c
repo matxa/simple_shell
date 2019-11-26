d513ef041d3bbabe405300db4b24a01dce357c3e
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-26 02:07:23 +0000

    add description for user_input

--- user_input.c
+++ user_input.c
@@ -1,17 +1,16 @@
 #include "simpleshell.h"
-
+/**
+ * user_input - gets user input and send to stdin
+ * Return: buffer, pointer
+ */
 char *user_input(void)
 {
 	char *buffer = NULL;
 	size_t bsize = 0;
-	/* check for get line */
 	int count_char;
 
 	count_char = getline(&buffer, &bsize, stdin);
 	if (count_char == -1)
 		perror("getline() failed ( count_char == -1 )");
-
-	/* count_char = _getline(&buffer); */
-
 	return (buffer);
 }
