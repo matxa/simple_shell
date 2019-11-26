f4b6569c944ec125f486ca91667c9634252847a5
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-26 19:43:36 +0000

    add exit

--- shell_main.c
+++ shell_main.c
@@ -1,27 +1,37 @@
 #include "simpleshell.h"
-
+/**
+ * shell_exit - exits process
+ * @argv: prompt vector
+ * Return: some return
+ */
+void shell_exit(char **argv)
+{
+	free(argv);
+	exit(EXIT_SUCCESS);
+}
 /**
 * main - main shell function, where the magic happens
 * takes in no arg
 * Return: return (0) on success
 */
-
-int main(void)
+int main(int ac, char **arg)
 {
-	/* the dollar :)*/
 	char *prompt = "$ ";
-
 	char *buffer;
 	char **cmd;
+	(void) (ac);
 
 	while (1)
 	{
 		write(1, prompt, (sizeof(char) * 2));
 		buffer = user_input();
 		cmd = tokens(buffer);
-		_execute(cmd);
+		run(cmd);
 
-		/* free(buffer);*/
-		/* free(cmd);*/
+		free(buffer);
+		free(cmd);
 	}
+	while (arg != NULL)
+		if (_strcmp(*arg, "exit") == 0)
+			shell_exit(cmd);
 }
