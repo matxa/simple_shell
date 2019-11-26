c564d4ddffe13c3b5076608014ee16302f60eb09
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2019-11-26 04:40:44 +0000

    filter function

--- execute.c
+++ execute.c
@@ -1,38 +1,46 @@
 #include "simpleshell.h"
 /**
- * _exit - exits process
- * @argc: prompt number
+ * shell_exit - exits process
  * @argv: prompt vector
- * return: some return
+ * Return: some return
  */
-int _exit(char **argv)
+void shell_exit(char **argv)
 {
-	free(*argv);
 	free(argv);
 	exit(EXIT_SUCCESS);
-	return (1);
 }
 /**
  * _execute - filters if builtin or not
+ * @args: user input
  * Return: run(cmd)
  */
 int _execute(char **args)
 {
 	int type;
-	char *cmd[] = {"env", "exit", NULL};
-	
-	if (args[0] == NULL)
+	char *cmd[4] = {"", "env", "exit", NULL};
+
+	type = 0;
+	if (args[type] == NULL)
 		return (1);
 	/* filter */
 	while (cmd[type] != NULL)
 	{
-		if(_strcmp(cmds[type], args[0]) == 0)
+		if (_strcmp(cmd[type], args[0]) == 0)
 			break;
 		type++;
 	}
 	/* call to cmds */
 	switch (type)
 	{
-		case 0:
-			_exit(args);
+		case (1):
+			_getenv(*args);		
+			break;
+		case (2):
+			shell_exit(args);
+			break;
+		default:
+			run(args);
+			break;
+	}
+	return (type);
 }
